1990c51f9f4955f96c161affed87e5f99e042e04
Marcus Paeschke <marcus@mphasize.de>
2014-07-22 17:39:00 +0200

    move .editorconfig into root directory and adjust whitespace in services/Ember.js

--- services/Ember.js
+++ services/Ember.js
@@ -5,25 +5,25 @@
  */
 
 var _ = require( 'lodash' ),
-	pluralize = require( 'pluralize' );
+  pluralize = require( 'pluralize' );
 
 module.exports = {
-	linkAssociations: function ( model, records ) {
-		if ( !Array.isArray( records ) ) records = [ records ];
-		var modelPlural = pluralize( model.identity );
+  linkAssociations: function ( model, records ) {
+    if ( !Array.isArray( records ) ) records = [ records ];
+    var modelPlural = pluralize( model.identity );
 
-		return _.map( records, function ( record ) {
-			var links = {};
-			_.each( model.associations, function ( assoc ) {
-				if ( assoc.type == "collection" ) {
-					links[ assoc.alias ] = sails.config.blueprints.prefix + "/" + modelPlural + "/" + record.id + "/" + assoc.alias;
-				}
-			} );
-			if ( _.size( links ) > 0 ) {
-				record.links = links;
-			}
-			return record;
-		} );
-	}
+    return _.map( records, function ( record ) {
+      var links = {};
+      _.each( model.associations, function ( assoc ) {
+        if ( assoc.type == "collection" ) {
+          links[ assoc.alias ] = sails.config.blueprints.prefix + "/" + modelPlural + "/" + record.id + "/" + assoc.alias;
+        }
+      } );
+      if ( _.size( links ) > 0 ) {
+        record.links = links;
+      }
+      return record;
+    } );
+  }
 
 };
