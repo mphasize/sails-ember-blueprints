a4eaedebfc60f4bd6f82b3cbebdab6de046e2bf9
Globegitter <markus.padourek@gmail.com>
2014-09-20 21:50:01 +0100

    Automatic copying of files and added peerDependencies.

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
-console.log("");
-console.log("Sorry, automatic installation is not yet implemented.");
-console.log("For now, just copy the blueprints/ folder into your Sails projects api/ directory and the services/Ember.js file into api/services/");
-console.log("");
-console.log("Example:");
-console.log("cp -R blueprints ../api/");
-console.log("cp services/* ../api/services/");
-console.log("");
+var fs = require('fs-extra')
+
+console.log("Copying the blueprints folder over");
+fs.copySync('blueprints/', '../../api/blueprints/');
+console.log("Copying the services folder over");
+fs.copySync('services/', '../../api/services/');
+console.log("Copying done.")
+
 process.exit();
