1900bfaf23b9b142c48dbf3356dda49c63902957
Globegitter <markus.padourek@gmail.com>
2014-09-20 22:05:34 +0100

    Updated output messages and readme.

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 var fs = require('fs-extra')
 
-console.log("Copying the blueprints folder over");
+console.log("Copying the blueprints folder into api/blueprints");
 fs.copySync('blueprints/', '../../api/blueprints/');
-console.log("Copying the services folder over");
+console.log("Copying the services folder into api/services");
 fs.copySync('services/', '../../api/services/');
 console.log("Copying done.")
 
